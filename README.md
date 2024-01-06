094726d6057c70df74e440ae009bf7e3a78c5ef2
Mark Perryman <tazmancoder@gmail.com>
2024-01-06 08:51:47 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,103 @@
+# ViewModifierKit
+
+<p>
+    <img src="https://img.shields.io/badge/iOS-13.0+-blue.svg" />
+    <img src="https://img.shields.io/badge/macOS-10.15-gold.svg" />
+    <img src="https://img.shields.io/badge/Swift-5.0-ff69b4.svg" />
+</p>
+
+----
+
+> This package contains all the code to be able to add padding to any
+SwiftUI view. The modifiers have an argument called `howMuch` if left
+out then the modifier defaults to 10
+
+----
+
+## Features
+
+### Padding Modifiers
+-   bottomPadding
+-   topPadding
+-   allPadding
+-   trailingPadding
+-   leadingPadding
+-   horizontalPadding
+-   verticalPadding
+
+#### Example
+
+```swift
+@State private var selectedIndex = 0
+
+var body: some View {
+    Text("Bottom padding applied")
+        .bottomPadding(howMuch: 5)
+
+    Text("Top padding applied")
+        .topPadding(howMuch: 5)
+        
+    Text("Generic padding applied")
+        .allPadding(howMuch: 5)
+        
+    Text("Trailing padding applied")
+        .trailingPadding(howMuch: 5)
+        
+    Text("Leading padding applied")
+        .leadingPadding(howMuch: 5)
+        
+    Text("Horizontal padding applied")
+        .horizontalPadding(howMuch: 5)
+        
+    Text("Vertical padding applied")
+        .verticalPadding(howMuch: 5)
+}
+```
+
+### Text View Modifiers
+<ul>
+<li>textViewModifier(for: , weight: , color: )
+<ul>
+<li>Font: Any Swift Font, Ex: .title</li>
+<li>Weight: Any font weight, Ex: .bold</li>
+<li>Color: Any SwiftUI Color, Ex: .primary</li>
+</ul>
+
+This modifier only needs the font provided the other arguments are optional and have default values.
+<ul>
+<li>Weight: Default value = .regular</li>
+<li>Color: Optional and if not passed in is just set to nil</li>
+</ul>
+
+#### Example
+
+```swift
+var body: some View {
+    Text("Bottom padding applied")
+        .textViewModifier(for: .body, weight: .semibold, color: .primary)
+}
+```
+
+## Installation
+
+The code is packaged as a framework. You can install manually (by copying the files in the `Sources` directory) or using Swift Package Manager (**preferred**)
+
+### Dependency For Another Swift Package
+To install using Swift Package Manager, add this to the `dependencies` section of your `Package.swift` file:
+`.package(url: "https://github.com/tazmancoder/ViewModifierKit.git", .upToNextMinor(from: "1.0.0"))`
+
+### Xcode project
+To install this into your Xcode project, follow the instructions below:
+
+1. Goto your project folder, tap Package Dependencies, under Packages click the plus button
+2. Enter this in searchbar - `https://github.com/tazmancoder/ViewModifierKit.git`. 
+3. Set `Dependency Rule` to `Up to Next Major Version`
+4. Tap `Add Package` button.
+
+Thats it this package will then be installed and ready to use.
+
+> Note: The package requires iOS v13.0+ or macOS v10.15+
+
+
+
+
---
+++ Sources/ViewModifierKit/ViewModifierKit.swift
@@ -0,0 +1,109 @@
+import SwiftUI
+
+public extension View {
+	/// Sets the bottom padding of a view
+	///
+	/// Controls the padding at the bottom of any
+	/// SwiftUI view.
+	///
+	/// - Parameter howMuch: How much padding
+	/// - Returns: A view with the padding applied
+	func bottomPadding(howMuch: CGFloat? = 10) -> some View {
+		padding(.bottom, howMuch)
+	}
+	
+	/// Sets the top padding of a view
+	///
+	/// Controls the padding at the top of any
+	/// SwiftUI view.
+	///
+	/// - Parameter howMuch: How much padding
+	/// - Returns: A view with the padding applied
+	func topPadding(howMuch: CGFloat? = 10) -> some View {
+		padding(.top, howMuch)
+	}
+	
+	/// Sets the padding of a view
+	///
+	/// Controls the padding at that gets applied
+	/// to all sides of the SwiftUI view.
+	///
+	/// - Parameter howMuch: How much padding
+	/// - Returns: A view with the padding applied
+	func allPadding(howMuch: CGFloat? = 10) -> some View {
+		padding(howMuch ?? 10)
+	}
+	
+	/// Sets the trailing padding of a view
+	///
+	/// Controls the padding at the trailing edge of any
+	/// SwiftUI view.
+	///
+	/// - Parameter howMuch: How much padding
+	/// - Returns: A view with the padding applied
+	func trailingPadding(howMuch: CGFloat? = 10) -> some View {
+		padding(.trailing, howMuch)
+	}
+	
+	/// Sets the leading padding of a view
+	///
+	/// Controls the padding at the leading edge of any
+	/// SwiftUI view.
+	///
+	/// - Parameter howMuch: How much padding
+	/// - Returns: A view with the padding applied
+	func leadingPadding(howMuch: CGFloat? = 10) -> some View {
+		padding(.leading, howMuch)
+	}
+	
+	/// Sets the horizontal padding of a view
+	///
+	/// Controls the padding at the horizontal of any
+	/// SwiftUI view. This padding is on both sides of
+	/// the view.
+	///
+	/// - Parameter howMuch: How much padding
+	/// - Returns: A view with the padding applied
+	func horizontalPadding(howMuch: CGFloat? = 10) -> some View {
+		padding(.horizontal, howMuch)
+	}
+	
+	/// Sets the vertical padding of a view
+	///
+	/// Controls the padding at the vertical of any
+	/// SwiftUI view. This padding is on both sides
+	/// of the view.
+	///
+	/// - Parameter howMuch: How much padding
+	/// - Returns: A view with the padding applied
+	func verticalPadding(howMuch: CGFloat? = 10) -> some View {
+		padding(.vertical, howMuch)
+	}
+}
+
+public extension Text {
+	
+	/// Modify's a text view
+	///
+	/// Some arguments like weight & color have default values and are optional. You only need
+	/// supply the font you want.
+	///
+	/// Usage:
+	/// ```
+	/// Text("Some Text")
+	///     .genericTextModifier(for: .subheadline, weight: .regular, color: .secondary)
+	/// ```
+	/// - Parameters:
+	///   - font: Any system font
+	///   - weight: Weight for font
+	///   - color: What color the text should be
+	/// - Returns: A new text view with the modifiers applied
+	func textViewModifier(for font: Font, weight: Font.Weight = .regular, color: Color? = nil) -> Text {
+		self
+			.font(font)
+			.fontWeight(weight)
+			.foregroundColor(color)
+	}
+}
+
+
